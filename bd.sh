3293926d7092509b4f4b4f10e4be8b85e910e82a
yx-ren <yunxiang.r@gmail.com>
2021-01-18 13:49:58 +0800

    update build sh

--- bd.sh
+++ bd.sh
@@ -1,21 +1,8 @@
 #/bin/bash
 
-UNAME=$(uname)
-function get_cpu_count()
-{
-    if [ ${UNAME} = "Darwin" ] ; then
-        CPU_COUNT=$(sysctl -n hw.ncpu)
-    else
-        CPU_COUNT=$(grep processor /proc/cpuinfo | wc -l)
-    fi
-}
-
-get_cpu_count
-if [ x$CPU_COUNT = x ] ; then
-   CPU_COUNT=4
-fi
-echo "cpu count:[${CPU_COUNT}]"
-
-build_cmd="make -j$CPU_COUNT > build.out 2>&1"
+build_cmd="build.sh > build.out 2>&1"
 echo "$build_cmd"
-make -j $CPU_COUNT > build.out 2>&1
+./build.sh > build.out 2>&1
+if [ $? != 0 ]; then
+    echo "build failed"
+fi
---
+++ mbd.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+UNAME=$(uname)
+function get_cpu_count()
+{
+    if [ ${UNAME} = "Darwin" ] ; then
+        CPU_COUNT=$(sysctl -n hw.ncpu)
+    else
+        CPU_COUNT=$(grep processor /proc/cpuinfo | wc -l)
+    fi
+}
+
+get_cpu_count
+if [ x$CPU_COUNT = x ] ; then
+   CPU_COUNT=4
+fi
+echo "cpu count:[${CPU_COUNT}]"
+
+build_cmd="make -j$CPU_COUNT > build.out 2>&1"
+echo "$build_cmd"
+make -j $CPU_COUNT > build.out 2>&1
+if [ $? != 0 ]; then
+    echo "build failed"
+fi
