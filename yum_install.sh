59e382dbbfc2ddef5493ea05022acea8f5e62a3e
meixue <renyunxiang.ryx@antgroup.com>
2021-11-02 10:20:59 +0800

    update yum install

--- yum_install.sh
+++ yum_install.sh
@@ -1,28 +1,33 @@
 #/bin/bash
-repo_list=(
-vim
-tcpdump
-strace
-systool
-lsof
-ss
-bash-c*
-tmux
-cargo
-rust
-glibc-common
-epel-release
-the_silver_searcher
-openssl
-openssl-devel
-libevent
-libevent-devel
-ncurses
-ncurses-devel
-)
+repos="\
+    vim
+    tcpdump
+    strace
+    systool
+    lsof
+    ss
+    bash-c*
+    tmux
+    cargo
+    rust
+    glibc-common
+    epel-release
+    the_silver_searcher
+    openssl
+    openssl-devel
+    libevent
+    libevent-devel
+    ncurses
+    ncurses-devel
+    ruby ruby-devel lua lua-devel luajit
+    luajit-devel ctags git python python-devel
+    python36 python36-devel tcl-devel
+    perl perl-devel perl-ExtUtils-ParseXS
+    perl-ExtUtils-XSpp perl-ExtUtils-CBuilder
+    perl-ExtUtils-Embed libX* ncurses-devel gtk2-devel
+"
 
-for repo in ${repo_list[@]}
-do
-    echo "install repo"
-    yum install -y $repo
-done
+echo $repos
+sudo yum install $repos
+
+exit 0
