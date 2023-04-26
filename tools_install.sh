b57b9e1181350de89cda37bd6b66bc15c062a27a
meixue <renyunxiang.ryx@antgroup.com>
2023-04-26 15:38:18 +0800

    add install vim8

--- tools_install.sh
+++ tools_install.sh
@@ -51,6 +51,43 @@ function install_tmux
     ./configure && make -j8
     mv /usr/bin/tmux /usr/bin/tmux.bak
     cp tmux /usr/bin/tmux
+
+    install_tmux_plugin
+}
+
+function install_tmux_plugin
+{
+    tmux_conf="~/.tmux.conf"
+    mkdir ~/.tmux
+    cd ~/.tmux
+    git clone https://github.com/tmux-plugins/tmux-resurrect.git
+    echo "run-shell ~/.tmux/tmux-resurrect/resurrect.tmux" >> ~/.tmux.conf
+    cd -
+    tmux source-file ~/.tmux.conf
+}
+
+function install_vim_8
+{
+    cd ${tmp_dir}
+    git clone git@github.com:vim/vim.git
+    cd vim
+	./configure --with-features=huge \
+	--enable-pythoninterp \
+	--enable-python3interp \
+	--enable-rubyinterp \
+	--enable-luainterp \
+	--enable-perlinterp \
+	--with-python-config-dir=/usr/lib64/python2.7/config/ \
+	--with-python3-config-dir=/usr/local/python3.6/lib/python3.6/config-3.6m-x86_64-linux-gnu/ \
+	--enable-cscope \
+	--enable-multibyte \
+	--prefix=/usr/local/vim
+
+	make -j8
+	make install
+
+	cp /usr/bin/vim /usr/bin/vim.bak
+	sudo ln -s /usr/local/vim/bin/vim /usr/bin/vim
 }
 
 function main
